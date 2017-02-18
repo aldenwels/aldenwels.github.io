dad24a084c90577a09a19b82f5781ba66dd69293
aldenwels <phil.gringer@gmail.com>
2017-02-18 13:26:12 -0500

    rearranged structure

--- js/git.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){ 
-$('#home').hover(function() {
-  $(this).attr('src', 'img/about-face-move.gif');
-}, function() {
-  $(this).attr('src', 'img/one.png');
-});
-});
--- js/script.js
+++ js/script.js
@@ -1,129 +1,28 @@
 $(function(){
-$(document).foundation();
-
-    var imageObj = {};
-    var divContainer = document.getElementById('canvasdiv'),
-    canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d'),
-    imgHeight = 500,
-    imgWidth = 500,
-    imageLoader = document.getElementById('imageLoader'),
-    img = new Image(),
-    xPos = 0,
-    yPos = 0,
-    imageData,
-    data,
-    originalData;
-
-    canvas.width = 500;
-    canvas.height = 500;
-    divContainer.appendChild(canvas);
-
-    var draw = function(){
-        ogimageData = ctx.getImageData(xPos,yPos,imgWidth,imgHeight);
-        ctx.drawImage(img,xPos,yPos,imgWidth,imgHeight);
-        ogdata = ogimageData.data;
-        }
-
-    var clearCanvas = function(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-    }
-
-
-    imageLoader.addEventListener('change',function(e){
-        var reader = new FileReader();
-        reader.onload = function(event){
-            img.onload = function(){
-                draw();
-            }
-            img.src = event.target.result;
-            }   
-            reader.readAsDataURL(e.target.files[0]); 
-        });
-    
-
-    var rangeSlider = function(){
-  var slider = $('.range-slider'),
-      range = $('.range-slider__range'),
-      value = $('.range-slider__value');
-    
-  slider.each(function(){
-
-    value.each(function(){
-      var value = $(this).prev().attr('value');
-      $(this).html(value);
-    });
-
-    range.on('input', function(){
-      $(this).next(value).html(this.value);
-    });
-  });
-};
-
-rangeSlider();
-
-
-var setOriginalData = function(){
-    for(var i = 0; i < data.length; i+= 4){
-        originalData[i] = data[i];
-        originalData[i+1] = data[i+1];
-        originalData[i+2] = data[i+2];
-    }
-}
+  var projects = $("textarea").val();
+  projects = JSON.parse(projects);
+  console.log(projects);
+  var page = 1;
+  for(var i = 0; i < projects.length;i++){
+    var imageLink;
+    if(projects[i].url != "")
+      imageLink = projects[i].url;
+    else
+      imageLink = projects[i]['github'];
 
 
 
-    /* 
-    panel 1
-    */
 
-    $("#widthControl").on('change',function(){
-        imgWidth = $(this).val();
-        clearCanvas();
-        draw();
-    });
-
-    $("#heightControl").on('change',function(){
-        imgHeight = $(this).val();
-        clearCanvas();
-        draw();
-    });
-
-    $("#xPos").on('change',function(){
-        xPos = $(this).val();
-        clearCanvas();
-        draw();
-    });
-
-     $("#yPos").on('change',function(){
-        yPos = $(this).val();
-        clearCanvas();
-        draw();
-    });
-
-     $("#reset-panel1").click(function(){
-        $("#yPos").val(0);
-        $("#xPos").val(0);
-        $("#widthControl").val(500);
-        $("#heightControl").val(500);
-        clearCanvas();
-        draw();
-        console.log("reset");
-     });
-
-    /* panel1 end */
+    $(".projects").append("<div class='large-6 small-12 columns end'>" +
+    "<a href='" + imageLink + "'>" + "<img src='" + projects[i].image +
+    "'></img></a><h2>" + projects[i].name + "</h2><p>" + projects[i].description +
+    "</p>");
 
 
-    $("#brigthnessControl").on('change',function(){
-        imageData = ctx.getImageData(xPos,yPos,imgWidth,imgHeight);
-        ctx.putImageData(ogimageData,xPos,yPos);
-        var amount = $(this).val();
-        for(var i = 0; i < ogdata.length;i+=4){
-            alteredData[i] += Number(amount);
-            alteredData[i+1] += Number(amount);
-            alteredData[i+2] += Number(amount);
-        }
-        ctx.putImageData(alteredImageData,xPos,yPos);
-    });
-        
+
+    $(".projects").append("</div>");
+
+
+
+  }
 });
